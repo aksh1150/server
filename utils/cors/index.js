33a7f46f2276e692f20145c45c10b9486850cbb8
Akshit Patel <aksh1150@gmail.com>
2020-11-23 09:03:04 -0500

    Updated

--- utils/cors/index.js
+++ utils/cors/index.js
@@ -4,8 +4,8 @@ const router = express.Router();
 router.use(
   cors({
     origin: [
-      "https://stupefied-clarke-dab412.netlify.app/",
-      "http://stupefied-clarke-dab412.netlify.app/",
+      "https://stupefied-clarke-dab412.netlify.app",
+      "http://stupefied-clarke-dab412.netlify.app",
       "https://api.sendgrid.com/v3/",
     ],
     methods: ["GET", "POST", "PUT"],
